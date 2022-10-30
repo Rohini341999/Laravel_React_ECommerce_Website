78c1f82c7912ff0260c6f531ffdd49de288d9c27
RohiniYipli <110519506+RohiniYipli@users.noreply.github.com>
2022-10-30 18:18:41 +0530

    registration done with backend as laravel sanctum and phpmyadmin Mysql database

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import axios from "axios";
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/frontend/auth/Login";
 import Register from "./components/frontend/auth/Register";
 import Home from "./components/frontend/Home";
 import MasterLayout from "./layouts/admin/MasterLayout";
+
+axios.defaults.baseURL = "http://localhost:8000/";
+axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.headers.post["Accept"] = "application/json";
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
--- src/components/frontend/auth/Register.js
+++ src/components/frontend/auth/Register.js
@@ -1,12 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
+import Navbar from "../../../layouts/frontend/Navbar";
+import axios from "axios";
+import swal from "sweetalert";
+import { useHistory } from "react-router-dom";
 
 function Register() {
+  const history = useHistory();
+  const [registerInput, setRegister] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error_list: [],
+  });
+
+  const handleInput = (e) => {
+    e.persist();
+    setRegister({ ...registerInput, [e.target.name]: e.target.value });
+  };
+
+  const registerSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      name: registerInput.name,
+      email: registerInput.email,
+      password: registerInput.password,
+    };
+
+    axios.get("/sanctum/csrf-cookie").then((response) => {
+      axios.post(`api/register`, data).then((res) => {
+        if (res.data.status === 200) {
+          localStorage.setItem("auth_token", res.data.token);
+          localStorage.setItem("auth_name", res.data.username);
+          swal("Success", res.data.message, "success");
+          history.push("/");
+        } else {
+          setRegister({
+            ...registerInput,
+            error_list: res.data.validation_errors,
+          });
+        }
+      });
+    });
+  };
+
   return (
     <div>
-      <h1>Register</h1>
-      <button type="" className="btn btn-primary">
-        Checking
-      </button>
+      <Navbar />
+      <div className="container py-5">
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <div className="card">
+              <div className="card-header">
+                <h4>Register</h4>
+              </div>
+              <div className="card-body">
+                <form onSubmit={registerSubmit}>
+                  <div className="form-group mb-3">
+                    <label> Full Name</label>
+                    <input
+                      onChange={handleInput}
+                      type=""
+                      name="name"
+                      value={registerInput.name}
+                      className="form-control"
+                    ></input>
+                    <span>{registerInput.error_list.name}</span>
+                  </div>
+                  <div className="form-group mb-3">
+                    <label> Email ID</label>
+                    <input
+                      onChange={handleInput}
+                      type=""
+                      name="email"
+                      value={registerInput.email}
+                      className="form-control"
+                    ></input>
+                    <span>{registerInput.error_list.email}</span>
+                  </div>
+                  <div className="form-group mb-3">
+                    <label> Password</label>
+                    <input
+                      onChange={handleInput}
+                      type=""
+                      name="password"
+                      value={registerInput.password}
+                      className="form-control"
+                    ></input>
+                    <span>{registerInput.error_list.password}</span>
+                  </div>
+
+                  <div className="form-group mb-3">
+                    <button type="submit" className="btn btn-primary">
+                      Register
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
